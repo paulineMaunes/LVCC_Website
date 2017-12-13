f7c994813d263e7585bc6c10c4fa19870a179abe
paulineMaunes <maunes.pauline@gmail.com>
2017-12-13 17:51:05 +0800

    Update script.js

--- assets/mobirise/js/script.js
+++ assets/mobirise/js/script.js
@@ -1,8 +1,9 @@
-   $('.navbar .dropdown').hover(function() {
+  $('.navbar .dropdown').hover(function() {
   $(this).find('.dropdown-menu').first().stop(true, true).delay(50).fadeIn();
 }, function() {
   $(this).find('.dropdown-menu').first().stop(true, true).delay(50).slideUp()
 });
+}
 
  $(window).load(function() {
 		// Animate loader off screen
